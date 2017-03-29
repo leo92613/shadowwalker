c4811b1200690cf323f6192d6e9116fe48c65c54
indecisivedragon <aleonai@gmail.com>
2017-03-28 20:15:12 -0400

    added test level

--- Assets/Prototype/CharMove.cs
+++ Assets/Prototype/CharMove.cs
@@ -21,27 +21,27 @@ public class CharMove : MonoBehaviour
 		isPressed = false;
 	    ani.enabled = true;
 	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		ani.enabled = true;
-		isPressed = false;
-		anchor.localPosition = new Vector3 ();
-		if (Input.GetKey (KeyCode.DownArrow)) {
-			isPressed = true;
-			anchor.localPosition = new Vector3 (0, -0.05f, 0);
-		}
-		if (Input.GetKey (KeyCode.UpArrow)) {
+
+    // Update is called once per frame
+    void Update()
+    {
+        ani.enabled = true;
+        isPressed = false;
+        anchor.localPosition = new Vector3();
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s")) {
+            isPressed = true;
+            anchor.localPosition = new Vector3(0, -0.05f, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w")) {
 			isPressed = true;
 			anchor.localPosition = new Vector3 (0, 0.05f, 0);
 		}
-		if (Input.GetKey (KeyCode.LeftArrow)) {
+		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey("a")) {
 			anchor.localPosition = new Vector3 (-0.05f, 0, 0);
 			isPressed = true;
 			sprite.flipX = true;
 		}
-		if (Input.GetKey (KeyCode.RightArrow)) {
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey("d")) {
 			anchor.localPosition = new Vector3 (0.05f, 0, 0);
 			isPressed = true;
 			sprite.flipX = false;
