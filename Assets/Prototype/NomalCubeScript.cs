b35f30f38dcb1b2876e996682d48aff16eed007a
indecisivedragon <aleonai@gmail.com>
2017-04-04 18:52:55 -0400

    level 1 update

---
+++ Assets/Prototype/NomalCubeScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NomalCubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //GetComponent<MeshRenderer>().material.SetColor("_TintColor", Color.blue);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
