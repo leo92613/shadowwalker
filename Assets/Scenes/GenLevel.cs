ba888a79acc42cd751d56d6b3f77aa02eb8391c7
Brian Quinn <bjq207@nyu.edu>
2017-04-08 01:38:26 -0400

    attempting PCG and level creation

---
+++ Assets/Scenes/GenLevel.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenLevel : MonoBehaviour {
+	public GameObject L;
+	public GameObject Check;
+	public GameObject Cube;
+	public GameObject RotCube;
+	// Use this for initialization
+	void Start () {
+		Instantiate(L, new Vector3(0, 0, -15), Quaternion.identity);
+		int lastDirection = 0;
+		int x = 0;
+		int y = 0;
+		int z = 0;
+		int dir = 0;
+		for (int i = 0; i<7;++i) {
+			int num = Random.Range (2, 6);
+			while (lastDirection == dir%3) {
+				dir = Random.Range (1, 7);
+			}
+			Instantiate (Check, new Vector3 (x, y, z), Quaternion.identity);
+			for (int k = 1; k < num; k++) {
+				if (dir == 1) {
+					x++;
+				} else if (dir == 2) {
+					y++;
+				} else if (dir == 3) {
+					z++;
+				} else if (dir == 4) {
+					x--;
+				} else if (dir == 5) {
+					y--;
+				} else if (dir == 6) {
+					z--;
+				}
+				Instantiate (Cube, new Vector3 (x, y, z), Quaternion.identity);
+			}
+			if (dir == 1) {
+				x++;
+			} else if (dir == 2) {
+				y++;
+			} else if (dir == 3) {
+				z++;
+			} else if (dir == 4) {
+				x--;
+			} else if (dir == 5) {
+				y--;
+			} else if (dir == 6) {
+				z--;
+			}
+			lastDirection = dir%3;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
